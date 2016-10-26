8fad86755ba36814c3c1541f2a95b6285f83202a
Edward Mason <esmason66@gmail.com>
2016-10-25 21:11:44 -0700

    trie file

---
+++ trie.py
@@ -0,0 +1,39 @@
+import sys
+import re
+
+        
+def read_parse_file(filename):
+	f = open(filename)
+	strings = []
+	for line in f:
+        	line = re.sub('\n', '', line)
+        	strings.append(line)
+	return strings
+
+def create_trie(strings, branchpoint, last_node_used):
+	if len(strings) == 0:
+        	return []
+	else:
+		letter = strings[0][0]
+                same_first_letter = []
+		for counter in range(len(strings)):
+                	if strings[0][0] == letter:
+				s = strings.pop(0)[1:]
+                        	if len(s) > 0:
+					same_first_letter.append(s)
+			else:
+				break
+		current_node = last_node_used + 1
+		left_child = create_trie(same_first_letter, current_node, current_node)
+		left_last_node_used = current_node if len(left_child) == 0 else left_child[-1][1]
+		return [ [branchpoint, current_node, letter] ] +left_child+ create_trie(strings, branchpoint, left_last_node_used)
+			
+		
+
+filename = re.sub("\n", "", sys.stdin.readline() )
+strings = read_parse_file(filename)
+sorted_strings = [s for s in sorted(strings)]
+trie = create_trie(sorted_strings, 1, 1)
+for elt in trie:
+        elt = [str(i) for i in elt]
+	print(" ".join(elt))
